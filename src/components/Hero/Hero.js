7b0ad507fd617d42706cc55319874ceee49405e1
Albert Koshy <albertkoshy@alberts-air.lan>
2021-11-04 13:08:48 -0700

    commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        Albert's Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+      Albert is a Front-End Developer with 4+ years of experience with React, Redux, HTML, CSS, Javascript and Wordpress. He has experience collaborating with cross functional teams, UI/UX designers, QA and BAs using Agile/SCRUM. Albert is a polished professional with excellent communication and team skills, attention to detail, and a passion to build high quality websites
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
---
+++ src/constants/constants.js
@@ -0,0 +1,29 @@
+export const projects = [
+  {
+    title: 'Beautiful Destinations',
+    description: "Using mainly React and React-Router, I created a mock travel website that showcases places around the world that myself and others would love to visit.",
+      image: '/images/travel-website-pic.png',
+      tags: ['React', 'React-Router', 'Styled Components'],
+    source: 'https://google.com',
+    visit: 'https://milkdudss.github.io/react-destination-site-v2/',
+    id: 0,
+  },
+  {
+    title: 'E-Commerce mockshop!',
+    description:"The reason this mockshop was created was for the sole purpose of practicing redux in a real world application .",
+    image: '/images/mockshop-pic.png',
+    tags: ['React', 'Redux'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  
+];
+
+export const TimeLineData = [
+  { year: 2016, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Founded JavaScript Mastery', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my own platform', },
+];
